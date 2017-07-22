5af94dfca86d64cd1b9b4470f863ef12cca83419
James Clarke <jamesraymondclarke@gmail.com>
2017-07-22 13:59:24 +0100

    Implementing Review factory and adding functionality to it in the profile controlller

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -3,8 +3,8 @@ angular
 .controller('ProfileCtrl', ProfileCtrl)
 .controller('EditProfileCtrl', EditProfileCtrl);
 
-ProfileCtrl.$inject = ['$auth', 'User', '$state'];
-function ProfileCtrl($auth, User, $state) {
+ProfileCtrl.$inject = ['$auth', 'User', '$state', 'Review'];
+function ProfileCtrl($auth, User, $state, Review) {
   const vm = this;
 
   // vm.user = User.query($state.params);
@@ -17,6 +17,30 @@ function ProfileCtrl($auth, User, $state) {
     $state.go('login');
   }
   vm.logout = logout;
+
+  vm.newReview = {};
+
+  function addReview() {
+    Review
+    .save({ user_id: vm.user.id }, vm.newReview)
+    .$promise
+    .then((review) => {
+      vm.user.reviews.push(review);
+      vm.newReview = {};
+    });
+  }
+  vm.addReview = addReview;
+
+  function deleteReview(review) {
+    Review
+    .delete({ user_id: vm.user.id, id: review.id })
+    .$promise
+    .then(() => {
+      const index = vm.user.reviews.indexOf(review);
+      vm.user.reviews.splice(index, 1);
+    });
+  }
+  vm.deleteReview = deleteReview;
 }
 
 EditProfileCtrl.$inject = ['$auth', 'User','$state'];
